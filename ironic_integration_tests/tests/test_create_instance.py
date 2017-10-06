6a4501410ff228ed628dba9ed8b1f7d56b3fb8b3
Melissa Kam <melissa.kam@rackspace.com>
2017-10-06 13:02:33 -0500

    Add reboot as verification step

--- ironic_integration_tests/tests/test_create_instance.py
+++ ironic_integration_tests/tests/test_create_instance.py
@@ -26,6 +26,7 @@ class InstanceTest(BaseTest):
         self.delete_cmd = "nova delete {0}"
 
     def _test_boot_instance(self, image):
+        # Create a key pair
         pubkey = self._random_name("testkey_")
         cmd = "ssh-keygen -f /tmp/{0} -N ''".format(pubkey)
         self.cli.execute_cmd(cmd)
@@ -34,6 +35,7 @@ class InstanceTest(BaseTest):
         self.cli.execute_cmd(cmd)
         self.resource_deletion.append("nova keypair-delete {0}".format(pubkey))
 
+        # Provision the instance
         name = self._random_name("test_boot_instance_")
         cmd = "nova boot --flavor {0} --image {1} --key-name {2} {3}".format(
             "", image, pubkey, name)
@@ -42,16 +44,19 @@ class InstanceTest(BaseTest):
         server_id = server.get("id")
         self.resource_deletion.append("nova delete {0}".format(server_id))
 
+        # Wait for instance to go to ACTIVE
         show_cmd = "nova show {0}".format(server_id)
         server = self.cli.wait_for_status(show_cmd, "status", "ACTIVE")
         self.assertEqual(server.get("status"), "ACTIVE")
         hv_id = server.get("OS-EXT-SRV-ATTR:hypervisor_hostname")
-        
+
+        # Verify hypervisor type of instance is ironic
         hv_cmd = "nova hypervisor-show {0}".format(hv_id)
         result = self.cli.execute_cmd(hv_cmd)
         hypervisor = parser.details(result)
         self.assertEqual(hypervisor.get("hypervisor_type"), "ironic")
 
+        # Verify can ssh into instance
         address_str = server.get("private network")
         addresses = address_str.split(",")
         ssh_address = None
@@ -64,10 +69,20 @@ class InstanceTest(BaseTest):
         result = self.cli.execute_w_retry(ssh_cmd)
         self.assertIn("cirros", result)
 
+        # Perform a server action (reboot) works and verify result
+        reboot_cmd = "nova reboot {0}".format(server_id)
+        self.cli.execute_cmd(reboot_cmd)
+        server = self.cli.wait_for_status(show_cmd, "status", "REBOOT")
+        self.assertEqual(server.get("status"), "REBOOT")
+        server = self.cli.wait_for_status(show_cmd, "status", "ACTIVE")
+        self.assertEqual(server.get("status"), "ACTIVE")
+        result = self.cli.execute_w_retry(ssh_cmd)
+        self.assertIn("cirros", result)
+
+        # Delete instance and verify deletion
         cmd = "nova delete {0}".format(server_id)
         self.cli.execute_cmd(cmd)
         self.cli.wait_for_deletion(show_cmd)
-
         result = self.cli.execute_cmd(show_cmd, fail_ok=True)
         self.assertIn("No server with a name or ID of", result)
 
