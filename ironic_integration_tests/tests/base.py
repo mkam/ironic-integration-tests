8d6674f417562041980718788bc0d698c65d27cb
Melissa Kam <melissa.kam@rackspace.com>
2017-10-17 13:34:15 -0500

    Update tests to use security groups

--- ironic_integration_tests/tests/base.py
+++ ironic_integration_tests/tests/base.py
@@ -48,8 +48,9 @@ class BaseTest(unittest.TestCase):
     def _create_instance(self, image, flavor, pubkey, name, network=""):
         if network != "":
             network = "--nic net-id={0}".format(network)
-        cmd = "nova boot --flavor {0} --image '{1}' --key-name {2} {3} {4}"\
-            .format(flavor, image, pubkey, name, network)
+        cmd = "nova boot --flavor {0} --image '{1}' --key-name {2} " \
+              "--security-group rpc-support,default {3} {4}"\
+            .format(flavor, image, pubkey, network, name)
         result = self.cli.execute_cmd(cmd)
         server = parser.details(result)
         server_id = server.get("id")
